662420a8d6d2d08e0305a6c870dc8d9c731da952
h3dema <henrique.duartemoura@imec.be>
2025-05-21 12:51:56 +0200

    add solar panel gen

---
+++ src/energy/model/basic-solar-panel-patterns.h
@@ -0,0 +1,20 @@
+#ifndef BASIC_SOLAR_PANEL_PATTERNS_H
+#define BASIC_SOLAR_PANEL_PATTERNS_H
+
+#include <vector>
+
+// Define the structure for pattern data
+struct SolarPattern {
+    int second;
+    double mean;
+    double std_dev;
+};
+
+
+#define SECONDS_IN_A_DAY 86400
+
+// Declare the function signature
+std::vector<SolarPattern> generate_day(int N = 1000);
+
+
+#endif // BASIC_SOLAR_PANEL_PATTERNS_H
