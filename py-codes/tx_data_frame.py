b37bcd9ba769d0fcd29b25e886aa4b545c8f83ed
h3dema <henrique.duartemoura@imec.be>
2025-08-01 11:01:57 +0200

    regex fix  + plot + docstrings

--- py-codes/tx_data_frame.py
+++ py-codes/tx_data_frame.py
@@ -1,32 +1,130 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import argparse
 import re
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from utilities import find_files
 
 
 def read_log(log_file):
-    # Load and parse log
+    """
+    Read a log file from the ns-3 script and parse its lines.
+
+    The log file is expected to contain lines with the following format:
+    StartTxDataFrame: time <float>, duration +<int>ns, size <int>, npackets <int>, cell <int>, node <int>
+
+    The time is the time in seconds when the transmission starts.
+    The duration is the duration of the transmission in nanoseconds.
+    The size is the size of the data frame in bytes.
+    The npackets is the number of packets in the data frame.
+    The cell is the number of the cell used for transmission.
+    The node is the number of the node which transmitted the data frame.
+
+    The function returns a pandas DataFrame with the following columns:
+    - duration_s: the duration of the transmission in seconds
+    - node: the number of the node which transmitted the data frame
+    - size: the size of the data frame in bytes
+    - npackets: the number of packets in the data frame
+    - cell: the number of the cell used for transmission
+    - time: the time in seconds when the transmission starts
+    """
+
     data = []
 
     # Regular expression to parse each line
-    pattern = r'StartTxDataFrame: size (\d+), npackets (\d+), duration \+(\d+)ns, node (\d+)'
+    pattern = r'StartTxDataFrame: time (.*), duration \+(\d+)ns, size (\d+), npackets (\d+), cell (\d+), node (\d+)'
 
+    # Load and parse log
     with open(log_file, 'r') as f:
         timestamp = 0  # initialize cumulative time in seconds
         for line in f:
             match = re.search(pattern, line)
             if match:
-                size = int(match.group(1))
-                npackets = int(match.group(2))
-                duration_ns = int(match.group(3))
-                node = int(match.group(4))
+                time = float(match.group(1))
+                duration_ns = int(match.group(2))
                 duration_s = duration_ns / 1e9
-                timestamp += duration_s  # accumulate time
-                data.append({'time': timestamp, 'node': node, 'size': size, 'npackets': npackets})
+                size = int(match.group(3))
+                npackets = int(match.group(4))
+                cell = int(match.group(5))
+                node = int(match.group(6))
+
+                data.append({'duration_s': duration_s, 'node': node, 'size': size, 'npackets': npackets, 'cell': cell, 'time': time})
 
     # Create DataFrame
     df = pd.DataFrame(data)
 
     return df
+
+
+
+def plot_tx_data_frame(log_file, show=False, xmin=None, xmax=None):
+    """
+    Plot data frame from given log file.
+
+    Parameters
+    ----------
+    log_file : str
+        Name of the log file containing the data to plot.
+    show : bool, optional
+        If True, show the plot. Otherwise, save to a file.
+    xmin : float, optional
+        Minimum x-axis value (i.e. time) to plot. If None, set to 0.
+    xmax : float, optional
+        Maximum x-axis value (i.e. time) to plot. If None, set to max time in data.
+
+    Returns
+    -------
+    None
+    """
+    df = read_log(log_file)
+
+    N = len(df["node"].unique())
+
+    fig, axs = plt.subplots(N, 2, figsize=(10, 10), sharex=True)
+
+    for i, [node, group] in enumerate(df.groupby('node')):
+        # Plot npackets
+        axs[i, 0].plot(group['time'], group['npackets'], drawstyle='steps-post', label=f'Node {node}')
+
+        # Plot size
+        axs[i, 1].plot(group['time'], group['size'], drawstyle='steps-post', linestyle='--', label=f'Node {node}')
+
+        if i == 0:
+            axs[i, 0].set_title(f'Number of Packets Over Time\nNode {node}')
+            axs[i, 1].set_title(f'Packet Size (bytes) Over Time\nNode {node}')
+
+        else:
+            axs[i, 0].set_title(f'Node {node}')
+            axs[i, 1].set_title(f'Node {node}')
+
+        if i == N - 1:
+            axs[i, 0].set_xlabel('Time (s)')
+            axs[i, 1].set_xlabel('Time (s)')
+
+    for ax in axs.flatten():
+        # ax.legend()
+        ax.legend().set_visible(False)
+        ax.grid(True)
+
+   # define the x-axis limits (window for plotting)
+    _min = 0 if xmin is None else max(0.0, xmin)
+    _max = df['time'].max() if xmax is None else min(df['time'].max(), xmax)
+    plt.xlim(_min, _max)
+
+    # ax1.set_ylabel('Number of Packets')
+    # ax2.set_ylabel('Packet Size (bytes)')
+
+    # ax1.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), title='Legend')
+    # ax2.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), title='Legend')
+
+    plt.tight_layout()
+    if show:
+        plt.show()
+    else:
+        img_fname = log_file.replace('.log', f'-{_min}-{_max}.png')
+        print("Saved plot to", img_fname)
+        plt.savefig(img_fname)
+    plt.close(fig)
 
 
 """
@@ -37,37 +135,26 @@ def read_log(log_file):
 """
 
 if __name__ == '__main__':
-    show = False
-    log_file = 'test.log'  # replace with your actual filename
-    df = read_log(log_file)
+    parser = argparse.ArgumentParser(description="Energy Changes")
+    parser.add_argument('-d', "--dir", type=str, default=None, help="Directory to search for the files")
+    parser.add_argument('-f', "--fname", type=str, default=None, help="Name of the file to process")
+    parser.add_argument("--show", action="store_true", help="Show the plot (otherwise save as PNG)")
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+    parser.add_argument("--min", type=float, default=None, help="Minimum time in seconds to consider for plotting")
+    parser.add_argument("--max", type=float, default=None, help="Maximum time in seconds to consider for plotting")
 
-    # Plot npackets
-    for node, group in df.groupby('node'):
-        ax1.plot(group['time'], group['npackets'], drawstyle='steps-post', label=f'Node {node}')
-    ax1.set_ylabel('Number of Packets')
-    ax1.set_title('Number of Packets Over Time')
-    ax1.legend()
-    ax1.grid(True)
+    args = parser.parse_args()
 
-    # Plot size
-    for node, group in df.groupby('node'):
-        ax2.plot(group['time'], group['size'], drawstyle='steps-post', linestyle='--', label=f'Node {node}')
-    ax2.set_xlabel('Time (s)')
-    ax2.set_ylabel('Packet Size (bytes)')
-    ax2.set_title('Packet Size Over Time')
-    ax2.legend()
-    ax2.grid(True)
+    if args.dir is not None:
+        log_files = find_files(args.dir, target_suffix="ns3_log_output.log")
+        print("Found log files:")
+        for file in log_files:
+            print(file)
 
-    ax1.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), title='Legend')
-    ax2.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), title='Legend')
+    elif args.fname is not None:
+        plot_tx_data_frame(args.fname, args.show, args.min, args.max)
 
-    plt.tight_layout()
-    if show:
-        plt.show()
     else:
-        img_fname = log_file.replace('.log', '.png')
-        print("Saved plot to", img_fname)
-        plt.savefig(img_fname)
-    plt.close()
+        print("Please provide either a directory or a file name to process.")
+        parser.print_help()
+        exit(1)
