36899c4a6a8ff9f4304f576673cd4b176ae49898
h3dema <henrique.duartemoura@imec.be>
2025-08-01 12:17:04 +0200

    new function is not good

--- py-codes/tx_data_frame.py
+++ py-codes/tx_data_frame.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import pandas as pd
 
 from utilities import find_files
@@ -84,10 +85,9 @@ def plot_tx_data_frame(log_file, show=False, xmin=None, xmax=None):
 
     for i, [node, group] in enumerate(df.groupby('node')):
         # Plot npackets
-        axs[i, 0].plot(group['time'], group['npackets'], drawstyle='steps-post', label=f'Node {node}')
-
+        axs[i, 0].vlines(x=group['time'], ymin=0, ymax=group['npackets'], label=f'Node {node}')
         # Plot size
-        axs[i, 1].plot(group['time'], group['size'], drawstyle='steps-post', linestyle='--', label=f'Node {node}')
+        axs[i, 1].vlines(x=group['time'], ymin=0, ymax=group['size'], label=f'Node {node}')
 
         if i == 0:
             axs[i, 0].set_title(f'Number of Packets Over Time\nNode {node}')
@@ -101,21 +101,19 @@ def plot_tx_data_frame(log_file, show=False, xmin=None, xmax=None):
             axs[i, 0].set_xlabel('Time (s)')
             axs[i, 1].set_xlabel('Time (s)')
 
+        axs[i, 0].set_ylim(0, df["npackets"].max() + 1)
+        axs[i, 1].set_ylim(0, df["size"].max() + 1)
+
     for ax in axs.flatten():
         # ax.legend()
         ax.legend().set_visible(False)
         ax.grid(True)
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))  # there is no fraction of packet or size
 
    # define the x-axis limits (window for plotting)
     _min = 0 if xmin is None else max(0.0, xmin)
     _max = df['time'].max() if xmax is None else min(df['time'].max(), xmax)
     plt.xlim(_min, _max)
-
-    # ax1.set_ylabel('Number of Packets')
-    # ax2.set_ylabel('Packet Size (bytes)')
-
-    # ax1.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), title='Legend')
-    # ax2.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), title='Legend')
 
     plt.tight_layout()
     if show:
@@ -126,6 +124,83 @@ def plot_tx_data_frame(log_file, show=False, xmin=None, xmax=None):
         plt.savefig(img_fname)
     plt.close(fig)
 
+
+def plot_tx_data_frame_step(log_file, show=False, xmin=None, xmax=None):
+    """
+        **NOTE**: We are not using this function, because it is very slow and compute intensive.
+    """
+    df = read_log(log_file)
+
+    N = len(df["node"].unique())
+
+    fig, axs = plt.subplots(N, 2, figsize=(10, 10), sharex=True)
+
+    for node_idx, [node, group] in enumerate(df.groupby('node')):
+        # Initialize step lists
+        step_times = []
+        step_sizes = []
+        step_npackets = []
+
+        # Add zero at t=0 if missing
+        if df.loc[0, 'time'] != 0:
+            step_times.append(0)
+            step_times.append(df.loc[0, 'time'])
+            step_npackets.append(0)
+            step_npackets.append(df.loc[0, 'npackets'])
+            step_sizes.append(0)
+            step_sizes.append(0)
+
+        # Build step signal
+        for i in range(len(df)):
+            t_start = df.loc[i, 'time']
+            s = df.loc[i, 'size']
+            p = df.loc[i, 'npackets']
+            t_end = t_start + df.loc[i, 'duration_s']
+
+            # Pulse start
+            step_times.append(t_start)
+            step_sizes.append(s)
+            step_npackets.append(p)
+
+            # Pulse end
+            step_times.append(t_end)
+            step_sizes.append(s)
+            step_npackets.append(p)
+
+            # Drop to zero (if not last pulse)
+            if i < len(df) - 1:
+                t_next = df.loc[i + 1, 'time']
+                step_times.append(t_end)
+                step_sizes.append(0)
+                step_npackets.append(0)
+
+                step_times.append(t_next)
+                step_sizes.append(0)
+                step_npackets.append(0)
+
+            # Plot
+            axs[node_idx, 0].step(step_times, step_npackets, where='post', color='blue')
+            axs[node_idx, 1].step(step_times, step_sizes, where='post', color='blue')
+
+    for ax in axs.flatten():
+        # ax.legend()
+        ax.legend().set_visible(False)
+        ax.grid(True)
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))  # there is no fraction of packet or size
+
+   # define the x-axis limits (window for plotting)
+    _min = 0 if xmin is None else max(0.0, xmin)
+    _max = df['time'].max() if xmax is None else min(df['time'].max(), xmax)
+    plt.xlim(_min, _max)
+
+    plt.tight_layout()
+    if show:
+        plt.show()
+    else:
+        img_fname = log_file.replace('.log', f'-{_min}-{_max}.png')
+        print("Saved plot to", img_fname)
+        plt.savefig(img_fname)
+    plt.close(fig)
 
 """
     This scripts reads a log file created from the NS_LOG.
@@ -153,6 +228,7 @@ if __name__ == '__main__':
 
     elif args.fname is not None:
         plot_tx_data_frame(args.fname, args.show, args.min, args.max)
+        # plot_tx_data_frame_step(args.fname, args.show, args.min, args.max)
 
     else:
         print("Please provide either a directory or a file name to process.")
