a96e6c1be69bac9dbf01c206c1101b38fb84c22b
h3dema <henrique.duartemoura@imec.be>
2025-11-12 18:42:58 +0100

    plot SNR

--- py-codes/get_snr.py
+++ py-codes/get_snr.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+from collections import defaultdict
 import pandas as pd
 
 from parse_ue_measurements import parse_measurements
@@ -29,9 +32,14 @@ def compute_snr_per_entry(data, noisefigure: dict, df_mapping: pd.DataFrame):
         time = entry["Time"]
 
         # Find IMSI from RNTI
-        imsi = df_mapping[df_mapping["RNTI"] == rnti]["IMSI"].values[0]
+        try:
+            imsi = int(df_mapping[df_mapping["RNTI"] == rnti]["IMSI"].values[0])
+        except IndexError:
+            # RNTI not found in mapping because it's not from a UE
+            continue
 
-        ue_params = noisefigure["UE"][imsi]
+        # Notice that IMSI starts in 1, and noisefigure["UE"] starts in 0
+        ue_params = noisefigure["UE"][imsi - 1]
         np = NoisePower(noise_figure_db=ue_params["NoiseFigure"], bandwidth_hz=bandwidth_hz)
         noise_dbm = np.get_noise_power_dbm()
 
@@ -49,24 +57,62 @@ def compute_snr_per_entry(data, noisefigure: dict, df_mapping: pd.DataFrame):
 
 
 
+def experiment_snr(path_log, verbose=False, save=True):
+    uelogfile = os.path.join(path_log, "ReportUeMeasurements.log")
+    noise_figure_file = os.path.join(path_log, "NoiseFigure.log")
+    connection_log_file = os.path.join(path_log, "cell_connection.log")
+
+    data = parse_measurements(uelogfile)
+    noisefigure = parse_noise_figure(noise_figure_file)
+    df_mapping = map_rnti_imsi_from_log(connection_log_file)  # pandas dataframe
+    # print(df_mapping)
+
+    snr_entries = compute_snr_per_entry(data, noisefigure, df_mapping)
+
+    # # print all entries
+    # for e in snr_entries:
+    #     print(f"Time {e['Time']:.1f}s | IMSI {e['IMSI']} | RSRP: {e['RSRP_dBm']:.2f} dBm | Noise: {e['Noise_dBm']:.2f} dBm | SNR: {e['SNR_dB']:.2f} dB")
+
+    # Group entries by IMSI to find constant SNR intervals
+    grouped = defaultdict(list)
+    for entry in snr_entries:
+        grouped[entry["IMSI"]].append(entry)
+
+    # Find constant SNR intervals
+    intervals = dict()
+    for imsi, records in grouped.items():
+        if imsi not in intervals:
+            intervals[imsi] = []
+        # print(f"IMSI {imsi}:")
+        start_time = records[0]["Time"]
+        current_snr = records[0]["SNR_dB"]
+
+        for i in range(1, len(records)):
+            if records[i]["SNR_dB"] != current_snr:
+                end_time = records[i - 1]["Time"]
+                print(f"  SNR {current_snr} dB from {start_time}s to {end_time}s")
+                intervals[imsi].append((start_time, end_time, current_snr))
+                # update the pointers
+                start_time = records[i]["Time"]
+                current_snr = records[i]["SNR_dB"]
+
+        # Print the last interval
+        end_time = records[-1]["Time"]
+        intervals[imsi].append((start_time, end_time, current_snr))
+        # print(f"  SNR {current_snr} dB from {start_time}s to {end_time}s")
+
+    if verbose:
+        print(intervals)
+
+    if save:
+        with open(os.path.join(path_log, "snr_intervals.pickle"), "wb") as f:
+            pickle.dump(intervals, f, protocol=pickle.DEFAULT_PROTOCOL)
+
+
 if __name__ == "__main__":
-    import os
     import argparse
     parser = argparse.ArgumentParser(description="Parse connection log file (map RNTI to IMSI).")
     parser.add_argument('-p', '--path-log', type=str, help="Path to the log files")
     args = parser.parse_args()
 
-    uelogfile = os.path.join(args.path_log, "ReportUeMeasurements.log")
-    noise_figure_file = os.path.join(args.path_log, "NoiseFigure.log")
-    connection_log_file = os.path.join(args.path_log, "cell_connection.log")
-
-    data = parse_measurements(uelogfile)
-    import pdb; pdb.set_trace()
-    noisefigure = parse_noise_figure(noise_figure_file)
-    # df_mapping = map_rnti_imsi_from_log(connection_log_file)  # pandas dataframe
-    df_mapping = pd.DataFrame([{"IMSI": 1, "RNTI": 0, "CellId": 0}])
-
-    snr_entries = compute_snr_per_entry(data, noisefigure, df_mapping)
-
-    for e in snr_entries:
-        print(f"Time {e['Time']:.1f}s | IMSI {e['IMSI']} | RSRP: {e['RSRP_dBm']:.2f} dBm | Noise: {e['Noise_dBm']:.2f} dBm | SNR: {e['SNR_dB']:.2f} dB")
+    experiment_snr(args.path_log, save=True)
